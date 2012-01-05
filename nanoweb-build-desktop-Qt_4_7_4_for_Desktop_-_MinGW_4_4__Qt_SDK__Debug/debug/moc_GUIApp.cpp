d03a7e72029b9a9ae014d683097746d763c84380
Stefan Bunk <stefan.bunk@googlemail.com>
2012-01-05 16:40:20 +0100

    Successful deployment to phone!

--- nanoweb-build-desktop-Qt_4_7_4_for_Desktop_-_MinGW_4_4__Qt_SDK__Debug/debug/moc_GUIApp.cpp
+++ nanoweb-build-desktop-Qt_4_7_4_for_Desktop_-_MinGW_4_4__Qt_SDK__Debug/debug/moc_GUIApp.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'GUIApp.h'
 **
-** Created: Thu 5. Jan 12:57:30 2012
+** Created: Thu 5. Jan 16:19:12 2012
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.4)
 **
 ** WARNING! All changes made in this file will be lost!
--- nanoweb-build-desktop-Qt_4_7_4_for_Desktop_-_MinGW_4_4__Qt_SDK__Debug/debug/moc_MenuApp.cpp
+++ nanoweb-build-desktop-Qt_4_7_4_for_Desktop_-_MinGW_4_4__Qt_SDK__Debug/debug/moc_MenuApp.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'MenuApp.h'
 **
-** Created: Thu 5. Jan 12:57:55 2012
+** Created: Thu 5. Jan 16:19:35 2012
 **      by: The Qt Meta Object Compiler version 62 (Qt 4.7.4)
 **
 ** WARNING! All changes made in this file will be lost!
