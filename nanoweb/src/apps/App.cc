33816123ff22c0a013ae1f2d482e9202974c2193
Stefan Bunk <stefan.bunk@googlemail.com>
2012-01-12 21:28:12 +0100

    Webseite geändert, damit leichter zu bearbeiten.

--- nanoweb/src/apps/App.cc
+++ nanoweb/src/apps/App.cc
@@ -4,58 +4,56 @@
 namespace ipn
 {
 
-	App::App(QWidget *parent) : QWidget(parent)
+App::App(QWidget *parent) : QWidget(parent)
+{
+		// Create a timer invoking the menu if the users hold a finger on the screen
+		// for a specific amount of time in milliseconds:
+		m_buttonHoldTimer = new QTimer(this);
+		m_buttonHoldTimer->setInterval(500);
+		m_buttonHoldTimer->setSingleShot(true);
+
+		// Invoke the marking menu on button clicks and on timer expiration:
+		connect(m_buttonHoldTimer, SIGNAL(timeout()), this, SLOT(triggerButtonHold()));
+
+		connect(this, SIGNAL(backButtonClickTriggered()), this, SLOT(backButtonClick()));
+		connect(this, SIGNAL(rightButtonClickTriggered()), this, SLOT(rightButtonClick()));
+		connect(this, SIGNAL(leftButtonClickTriggered()), this, SLOT(leftButtonClick()));
+
+		connect(this, SIGNAL(backButtonHoldTriggered()), this, SLOT(backButtonHold()));
+		connect(this, SIGNAL(rightButtonHoldTriggered()), this, SLOT(rightButtonHold()));
+		connect(this, SIGNAL(leftButtonHoldTriggered()), this, SLOT(leftButtonHold()));
+
+		// let's bind some overall actions
+		connect(this, SIGNAL(performPopApp()), parent, SLOT(popApp()));
+		connect(this, SIGNAL(rightButtonClickTriggered()), parent, SLOT(switchToMockUp()));
+		connect(this, SIGNAL(leftButtonClickTriggered()), parent, SLOT(switchToWebPage()));
+	}
+
+	void App::popApp()
 	{
+		emit performPopApp();
+	}
 
+	void App::triggerButtonHold()
+	{
+		if(m_holdingButton == Left)
+			emit leftButtonHoldTriggered();
+		else if(m_holdingButton == Right)
+			emit rightButtonHoldTriggered();
+		else if(m_holdingButton == Back)
+			emit backButtonHoldTriggered();
+		//else if(m_holdingButton == "Screen")
+		//    emit holdTriggered();
 
-            // Create a timer invoking the menu if the users hold a finger on the screen
-            // for a specific amount of time in milliseconds:
-            m_buttonHoldTimer = new QTimer(this);
-            m_buttonHoldTimer->setInterval(500);
-            m_buttonHoldTimer->setSingleShot(true);
-
-            // Invoke the marking menu on button clicks and on timer expiration:
-            connect(m_buttonHoldTimer, SIGNAL(timeout()), this, SLOT(triggerButtonHold()));
-
-            connect(this, SIGNAL(backButtonClickTriggered()), this, SLOT(backButtonClick()));
-            connect(this, SIGNAL(rightButtonClickTriggered()), this, SLOT(rightButtonClick()));
-            connect(this, SIGNAL(leftButtonClickTriggered()), this, SLOT(leftButtonClick()));
-
-            connect(this, SIGNAL(backButtonHoldTriggered()), this, SLOT(backButtonHold()));
-            connect(this, SIGNAL(rightButtonHoldTriggered()), this, SLOT(rightButtonHold()));
-            connect(this, SIGNAL(leftButtonHoldTriggered()), this, SLOT(leftButtonHold()));
-
-            // let's bind some overall actions
-            connect(this, SIGNAL(performPopApp()), parent, SLOT(popApp()));
-            connect(this, SIGNAL(rightButtonClickTriggered()), parent, SLOT(switchToMockUp()));
-            connect(this, SIGNAL(leftButtonClickTriggered()), parent, SLOT(switchToWebPage()));
-        }
-
-        void App::popApp()
-        {
-            emit performPopApp();
-        }
-
-        void App::triggerButtonHold()
-        {
-            if(m_holdingButton == Left)
-                emit leftButtonHoldTriggered();
-            else if(m_holdingButton == Right)
-                emit rightButtonHoldTriggered();
-            else if(m_holdingButton == Back)
-                emit backButtonHoldTriggered();
-            //else if(m_holdingButton == "Screen")
-            //    emit holdTriggered();
-
-            m_holdingButton = AllReleased;
-        }
+		m_holdingButton = AllReleased;
+	}
 
 	void App::handleGesture(GestureType type, qreal param)
 	{
 		switch (type)
 		{
 		case PinchIn:
-                        emit pinchInTriggered();
+			emit pinchInTriggered();
 			break;
 		case PinchOut:
 			emit pinchOutTriggered();
@@ -82,75 +80,75 @@ namespace ipn
 			emit swipeDownTriggered();
 			break;
 
-                case LeftButtonPress:
-                        m_buttonHoldTimer->start();
-                        m_holdingButton = Left;
-                        emit leftButtonPressTriggered();
-                        break;
-                case LeftButtonRelease:
-                        if(m_buttonHoldTimer->isActive()) {
-                            emit leftButtonReleaseTriggered();
-                            emit leftButtonClickTriggered();
-                        }
-                        m_buttonHoldTimer->stop();
-                        break;
+		case LeftButtonPress:
+			m_buttonHoldTimer->start();
+			m_holdingButton = Left;
+			emit leftButtonPressTriggered();
+			break;
+		case LeftButtonRelease:
+			if(m_buttonHoldTimer->isActive()) {
+				emit leftButtonReleaseTriggered();
+				emit leftButtonClickTriggered();
+			}
+			m_buttonHoldTimer->stop();
+			break;
 
-                case RightButtonPress:
-                        m_buttonHoldTimer->start();
-                        m_holdingButton = Right;
-                        emit rightButtonPressTriggered();
-                        break;
-                case RightButtonRelease:
-                        if(m_buttonHoldTimer->isActive()) {
-                            emit rightButtonReleaseTriggered();
-                            emit rightButtonClickTriggered();
-                        }
-                        m_buttonHoldTimer->stop();
-                        break;
+		case RightButtonPress:
+			m_buttonHoldTimer->start();
+			m_holdingButton = Right;
+			emit rightButtonPressTriggered();
+			break;
+		case RightButtonRelease:
+			if(m_buttonHoldTimer->isActive()) {
+				emit rightButtonReleaseTriggered();
+				emit rightButtonClickTriggered();
+			}
+			m_buttonHoldTimer->stop();
+			break;
 
-                case BackButtonPress:
-                        m_buttonHoldTimer->start();
-                        m_holdingButton = Back;
-                        emit backButtonPressTriggered();
-                        break;
-                case BackButtonRelease:
-                        if(m_buttonHoldTimer->isActive()) {
-                            emit backButtonReleaseTriggered();
-                            emit backButtonClickTriggered();
-                        }
-                        m_buttonHoldTimer->stop();
-                        break;
-                }
-        }
-        void App::backButtonClick()
-        {
-                emit popApp();
-                qDebug() << "Backbutton clicked! ";
-        }
+		case BackButtonPress:
+			m_buttonHoldTimer->start();
+			m_holdingButton = Back;
+			emit backButtonPressTriggered();
+			break;
+		case BackButtonRelease:
+			if(m_buttonHoldTimer->isActive()) {
+				emit backButtonReleaseTriggered();
+				emit backButtonClickTriggered();
+			}
+			m_buttonHoldTimer->stop();
+			break;
+		}
+	}
+	void App::backButtonClick()
+	{
+		emit popApp();
+		qDebug() << "Backbutton clicked! ";
+	}
 
-        void App::rightButtonClick()
-        {
-                qDebug() << "Rightbutton clicked! ";
-        }
+	void App::rightButtonClick()
+	{
+		qDebug() << "Rightbutton clicked! ";
+	}
 
-        void App::leftButtonClick()
-        {
-                qDebug() << "Leftbutton clicked! ";
-        }
+	void App::leftButtonClick()
+	{
+		qDebug() << "Leftbutton clicked! ";
+	}
 
-        void App::leftButtonHold()
-        {
-                qDebug() << "Leftbutton hold! ";
-        }
+	void App::leftButtonHold()
+	{
+		qDebug() << "Leftbutton hold! ";
+	}
 
-        void App::rightButtonHold()
-        {
-                qDebug() << "Rightbutton hold! ";
-        }
+	void App::rightButtonHold()
+	{
+		qDebug() << "Rightbutton hold! ";
+	}
 
-        void App::backButtonHold()
-        {
-                qDebug() << "Backbutton hold! ";
-        }
+	void App::backButtonHold()
+	{
+		qDebug() << "Backbutton hold! ";
+	}
 
 } // namespace ipn
